9e056309b00d8aa16b3d9168f80b24229838fff8
Aaron Dollard <S00152905@mail.itsligo.ie>
2020-02-05 01:01:29 +0000

    Added countries of the world

---
+++ Time_Travel/Countries.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time_Travel
+{
+    public class Country
+    {
+        public enum Zone {Africa, Asia, Europe, Latin_America_and_Carribean, Oceania, North_America }
+
+        //Properities
+        public string Name { get; set; }
+        public Zone CountryZone { get; set; }
+
+        //Constructor
+        public Country(string name, Zone countryzone)
+        {
+            Name = name;
+            CountryZone = countryzone;
+        }
+
+        //ToString for the ListBox of countries
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+    }
+}
