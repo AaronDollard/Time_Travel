d0e8209ca1803e619dec714fd9e3b035a0d7b6c9
Aaron Dollard <daronaollard1995@gmail.com>
2020-04-15 16:35:11 +0100

    additional changes to planner tab. Added events to buttons and exception handling

--- Time_Travel/MainWindow.xaml.cs
+++ Time_Travel/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data.SqlClient; //for the wildcard search
+using Microsoft.VisualBasic; //Used for pop up menu
 
 namespace Time_Travel
 {
@@ -41,70 +42,7 @@ namespace Time_Travel
             list_bx_world_countries.ItemsSource = country.ToList(); //sortCountries(); //Sorts the names of the countries.
         }
 
-        #region ListBoxes
-        private void bucket_add_Click(object sender, RoutedEventArgs e) //Add countries to the bucket list from all countries list
-        {
-            list_bx_world_bucket.Items.Add(list_bx_world_countries.SelectedItem);
-            list_bx_world_countries.Items.Remove(list_bx_world_countries.SelectedItem);
-
-            Country selectedCountry = list_bx_world_countries.SelectedItem as Country; //Grabbing the selected item
-            
-            if (selectedCountry != null) //Making sure something is selected
-            {
-                
-                allCountries.Remove(selectedCountry); //Moves country to the other list
-                selectedCountries.Add(selectedCountry);
-                sortCountries();
-                RefreshScreens();
-            }
-
-            else if (selectedCountry == null) //Warning if nothing is selected
-            {
-                MessageBox.Show("Please select an country to add to bucketlist!", "Attention!");
-            }
-        } 
-
-        private void bucket_remove_Click(object sender, RoutedEventArgs e)
-        {
-            Country selectedCountry = list_bx_world_bucket.SelectedItem as Country; //Grabbing the selected item
-
-            //Making sure something is selected
-            if (selectedCountry != null)
-            {
-                //Moves country to the other list
-                selectedCountries.Remove(selectedCountry);
-                allCountries.Add(selectedCountry);
-                sortCountries();
-                RefreshScreens();
-            }
-
-            if (selectedCountry == null)
-            {
-                MessageBox.Show("Please select an country to remove from bucketlist!", "Attention!");
-            }
-        } //Add countries back to the all countries from the bucket
-
-        private void bucket_visited_Click(object sender, RoutedEventArgs e)
-        {
-
-            Country selectedCountry = list_bx_world_bucket.SelectedItem as Country; //Grabbing the selected item
-
-            //Making sure something is selected
-            if (selectedCountry != null)
-            {
-                //Moves country to the other list
-                selectedCountries.Remove(selectedCountry);
-                visitedCountries.Add(selectedCountry);
-                sortCountries();
-                RefreshScreens();
-            }
-
-            if (selectedCountry == null)
-            {
-                MessageBox.Show("Please select a country you have visited!", "Attention!");
-            }
-        } //Add countries to the countries you have visited list
-
+        #region Filtering Countries and Sorting
         private void RefreshScreens() //The method below refreshes the countries and bucket screen
         {
             //list_bx_world_countries.ItemsSource = null;
@@ -116,8 +54,6 @@ namespace Time_Travel
             //list_bx_world_visited.ItemsSource = null;
             //list_bx_world_visited.ItemsSource = visitedCountries;
         }
-        #endregion
-
         private void world_filter_combo_SelectionChanged(object sender, SelectionChangedEventArgs e) //Filter for the combobox zones
         {
             
@@ -179,14 +115,11 @@ namespace Time_Travel
         list_bx_world_countries.ItemsSource = query.ToList(); //Update items list
 
         }
-
         private void sortCountries() //Sorts the countries method
         { 
             allCountries.Sort();
         }
-
         public enum Zone { All, Africa, Asia, Europe, Latin_America_and_Carribean, Oceania, North_America } //Enum for the combobox
-
         private void countries_search_TextChanged(object sender, TextChangedEventArgs e) //Used for the search bar function to filter countries only in the selected zone. 
         {                                                                                //One if acts as the all selection while the other filters it by zone
             string selectedfilter = world_filter_combo.SelectedItem as string;           
@@ -206,10 +139,76 @@ namespace Time_Travel
             }
             
         }
+        #endregion
 
+        #region Buttons & Clicking Events
         private void github_link_Click(object sender, RoutedEventArgs e) //Click to go to my Github
         {
             System.Diagnostics.Process.Start("https://github.com/AaronDollard/Time_Travel");
         }
+        private void bucket_edit_Click(object sender, RoutedEventArgs e) //used when clicked on the button to edit the explored tab
+        {
+            //string input = Interaction.InputBox("Enter the name of the country you visited?", "Country", "Country Name", -1, -1);
+        }
+        private void bucket_visited_Click(object sender, RoutedEventArgs e)//Adding countries you visited to the second tab
+        {
+            if (list_bx_world_bucket.SelectedIndex != -1)
+            {
+                list_bx_world_visited.Items.Add(list_bx_world_bucket.SelectedValue);
+                list_bx_world_bucket.Items.Remove(list_bx_world_bucket.SelectedValue);
+            }
+            
+            else
+            {
+                MessageBox.Show("Please select a country you've visited!", "Attention!");
+            }
+        }
+        private void bucket_add_Click(object sender, RoutedEventArgs e) //Add countries to the bucket list from all countries list
+        {
+            if (string.IsNullOrEmpty(country_visited_input.Text))
+            {
+                MessageBox.Show("Please enter a country to add to bucketlist!", "Attention!");
+            }
+            else
+            {
+                list_bx_world_bucket.Items.Add(country_visited_input.Text);
+                country_visited_input.Clear();
+                country_visited_input.Focus();
+            }
+        }
+        private void bucket_remove_Click(object sender, RoutedEventArgs e) //Add countries back to the all countries from the bucket
+        {
+            if (list_bx_world_bucket.Items.Count > 0)
+            {
+                list_bx_world_bucket.Items.RemoveAt(list_bx_world_bucket.SelectedIndex);
+            }
+            else
+            {
+                MessageBox.Show("Please select an country to remove!", "Attention!");
+            }
+        }
+        #endregion
+
+        /*private void bucket_visited_Click(object sender, RoutedEventArgs e)
+       {
+
+           Country selectedCountry = list_bx_world_bucket.SelectedItem as Country; //Grabbing the selected item
+
+           //Making sure something is selected
+           if (selectedCountry != null)
+           {
+               //Moves country to the other list
+               selectedCountries.Remove(selectedCountry);
+               visitedCountries.Add(selectedCountry);
+               sortCountries();
+               RefreshScreens();
+           }
+
+           if (selectedCountry == null)
+           {
+               MessageBox.Show("Please select a country you have visited!", "Attention!");
+           }
+       } //Add countries to the countries you have visited list*/ //Unused button.
+        
     }
 }
